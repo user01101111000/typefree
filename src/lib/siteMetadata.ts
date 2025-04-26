a0f1f54ec64cbb3abe51953f52982a33bf230250
sebuhiiiiii <sebuhiiiiii36@gmail.com>
2025-04-27 00:38:11 +0400

    fixing

--- src/lib/siteMetadata.ts
+++ src/lib/siteMetadata.ts
@@ -2,7 +2,7 @@ export const siteMetadata = {
     title: "Type Free - JSON to TypeScript Interface Generator",
     author: 'user01101111000',
     headerTitle: 'Type Free',
-    description: "This website is a tool to convert JSON data to TypeScript interface. User friendly and beautiful interface.",
+    description: "This website is a tool to convert JSON data to TypeScript interface. Free, open Source and user friendly interface.",
     language: 'en-us',
     theme: 'system',
     siteUrl: 'https://typefree.vercel.app/',
